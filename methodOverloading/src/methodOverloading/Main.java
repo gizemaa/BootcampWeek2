518155da7639bfc11ecaa8a86ce97bcf762613fb
gizemaa <48960032+gizemaa@users.noreply.github.com>
2022-09-28 18:07:19 +0300

    Add files via upload

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,15 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem= new DortIslem();
+		dortIslem.topla(23, 5);
+		System.out.println(dortIslem.topla(23, 5));
+		
+		dortIslem.topla(5, 8, 9);
+	
+
+	}
+
+}
